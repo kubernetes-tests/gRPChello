2966123a67e2b6f65a75e0e55427e07b7a215f94
jonas27 <joE@12345>
2019-04-03 09:25:07 +0200

    proto stuff

---
+++ src/helloClient/main.go
@@ -0,0 +1,37 @@
+package helloClient
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"log"
+	"os"
+	"time"
+)
+
+const (
+	address     = "localhost:50051"
+	defaultName = "hello"
+)
+
+func main() {
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := hello.NewGreeterClient(conn)
+
+	// Contact the server and print out its response.
+	name := defaultName
+	if len(os.Args) > 1 {
+		name = os.Args[1]
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.Hello(ctx, &pb.HelloRequest{Name: name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.Message)
+}
